f0d91267b689cbb6e7a07a456964b1ad6a47ad91
chen08209 <chen08209@gmail.com>
2026-05-16 03:25:22 +0800

    cache

--- lib/views/config/on_demand.dart
+++ lib/views/config/on_demand.dart
@@ -127,7 +127,7 @@ class _OnDemandViewState extends ConsumerState<OnDemandView> {
                       subtitle: Text(appLocalizations.batteryOptimizationDesc),
                       trailing: isLoading
                           ? const SizedBox(
-                              width: 120,
+                              width: 80,
                               child: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 mainAxisAlignment: MainAxisAlignment.end,
@@ -163,23 +163,18 @@ class _OnDemandViewState extends ConsumerState<OnDemandView> {
                       minVerticalPadding: 8,
                       title: Text(appLocalizations.locationPermission),
                       subtitle: Text(appLocalizations.locationPermissionDesc),
-                      trailing: Padding(
-                        padding: const EdgeInsets.only(left: 40),
-                        child: CommonMinFilledButtonTheme(
-                          child: FilledButton(
-                            style: FilledButton.styleFrom(
-                              backgroundColor: locationPermissionsGranted
-                                  ? null
-                                  : context.colorScheme.error,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 0,
-                              ),
-                              minimumSize: const Size(80, 40),
-                            ),
-                            onPressed: _handleRequestLocationPermission,
-                            child: Text(
-                              locationPermissionsGranted ? '已授权' : '点击授权',
-                            ),
+                      trailing: CommonMinFilledButtonTheme(
+                        child: FilledButton(
+                          style: FilledButton.styleFrom(
+                            backgroundColor: locationPermissionsGranted
+                                ? null
+                                : context.colorScheme.error,
+                            padding: const EdgeInsets.symmetric(horizontal: 0),
+                            minimumSize: const Size(80, 40),
+                          ),
+                          onPressed: _handleRequestLocationPermission,
+                          child: Text(
+                            locationPermissionsGranted ? '已授权' : '点击授权',
                           ),
                         ),
                       ),
